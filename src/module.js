645b81ca2fc094b790550b35d58afb594411df9a
Vasiliy Os <talk@vasiliy0s.com>
2014-10-07 19:10:21 +0400

    Initial commit of development with several TODOs.

---
+++ src/module.js
@@ -0,0 +1,36 @@
+
+this.createjs = this.createjs || {};
+
+// TODO: new SoundJSDirector.Group() - constructor of sound groups.
+// TODO: new SoundJSDirector.Sound() - contructor-wrapper for soun instances with depended by group settings methods.
+// TODO: new SoundJSDirector.Manager() - manager with scoped registered groups, sounds and package applyers.
+// TODO: events in group like single sound.
+// TODO: SoundJSDirector.setGroups() - register groups of sounds.
+// TODO: SoundJSDirector.setSounds() - register sounds (with initialization or loading).
+// TODO: Group.play(options) - play all sounds in group.
+// TODO: Group.stop(options) - -/-.
+// TODO: Group.pause() - -/-.
+// TODO: Group.setVolume() - -/-.
+// TODO: Group.getVolume() - -/-, etc.
+// TODO: autoinclude to PreloadJS as plugin if it possible.
+
+;(function (createjs) {
+  
+  'use strict';
+
+  var SoundJS = createjs.SoundJS = createjs.SoundJS || {};
+
+  function SoundJSDirector (/* config */) {
+    // TODO: initalize with @config.
+  }
+
+  createjs.SoundJSDirector = SoundJSDirector;
+
+  var DirectorProto = SoundJSDirector.prototype = new createjs.EventDispatcher();
+
+  DirectorProto._sendEvent = function (type) {
+    var event = new createjs.Event(type);
+    this.dispatchEvent(event);
+  };
+
+} (createjs));
