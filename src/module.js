fade09475bdd26640e3f1212586bb8017e8c2420
Vasiliy Os <talk@vasiliy0s.com>
2014-10-08 17:05:59 +0400

    Removed completed todos and useless code.

--- src/module.js
+++ src/module.js
@@ -1,9 +1,5 @@
 'use strict';
 
-// TODO: new SoundJSDirector.Group() - constructor of sound groups.
-// TODO: new SoundJSDirector.Sound() - contructor-wrapper for soun instances with depended by group settings methods.
-// TODO: new SoundJSDirector.Manager() - manager with scoped registered groups, sounds and package applyers.
-// TODO: events in group like single sound.
 // TODO: SoundJSDirector.setGroups() - register groups of sounds.
 // TODO: SoundJSDirector.setSounds() - register sounds (with initialization or loading).
 // TODO: Group.play(options) - play all sounds in group.
@@ -12,14 +8,11 @@
 // TODO: Group.setVolume() - -/-.
 // TODO: Group.getVolume() - -/-, etc.
 // TODO: autoinclude to PreloadJS as plugin if it possible.
-
-var SoundJS = createjs.SoundJS = createjs.SoundJS || {};
+// TODO: provide sounds events in group like 'playing', 'played', 'loaded', other...
 
 function SoundJSDirector (/* config */) {
   // TODO: initalize with @config.
 }
-
-SoundJS.Director = SoundJSDirector;
 
 createjs.SoundJSDirector = SoundJSDirector;
 
