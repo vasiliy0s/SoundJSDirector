892665c096f159fa676d672bcd964835d3168663
Vasiliy Os <talk@vasiliy0s.com>
2014-10-08 17:06:22 +0400

    Started groupped playing method

--- src/controls/playback.js
+++ src/controls/playback.js
@@ -1,7 +1,13 @@
 'use strict';
 
-SoundJSDirectorGroupProto.play = function playGroup () {
-  // TODO
+// TODO: add easings to all of current methods.
+
+SoundJSDirectorGroupProto.play = function playGroup (options) {
+  // TODO: Pass group options to every sounds considering every group of sound.
+  this.eachSound(function (sound) {
+    // TODO: pass arguments multiplied with all instance groups.
+    sound.play(options);
+  }, this);
   return this;
 };
 
