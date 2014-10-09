9a6cd2ff972ce05b7bc1f054c1a30135abd803c7
Vasiliy Os <talk@vasiliy0s.com>
2014-10-09 17:59:26 +0400

    Completed .setLoop() and .switchLoop() methods of SoundJSDirector.Group.

--- src/controls/loop.js
+++ src/controls/loop.js
@@ -1,6 +1,15 @@
 'use strict';
 
-SoundJSDirectorGroupProto.setLoop = function setGroupLoop () {
-  // TODO
+// Set @value for group.
+SoundJSDirectorGroupProto.setLoop = function setGroupLoop (value) {
+  this.options.loop = parseInt(value) || 0;
+  return this;
+};
+
+// Switch group loop to 0 if it not 0 and -1 or @value if it is.
+SoundJSDirectorGroupProto.switchLoop = function switchGroupLoop (value) {
+  var options = this.options,
+      loop = options.loop;
+  options.loop = loop === 0 ? (parseInt(value) || -1) : 0;
   return this;
 };
