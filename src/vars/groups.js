9bd44d6b505219a51804570fb159124e1ba8dde6
Vasiliy Os <talk@vasiliy0s.com>
2014-10-09 12:51:35 +0400

    Added indexing of sounds in groups.

--- src/vars/groups.js
+++ src/vars/groups.js
@@ -29,33 +29,59 @@ function getSoundGroups (sound) {
 // Add group to sound groups.
 SoundJSDirector.setSoundGroup = SoundJSDirector.prototype.setSoundGroup = 
 function setSoundGroup (sound, group) {
+
   if (!(sound && 'object' === typeof sound)) {
     return;
   }
+
   if ('string' === typeof group) {
     group = SoundJSDirector.group(group);
   }
-  var soundGroups = SoundJSDirector.getSoundGroups(sound);
+
+  var soundGroups = SoundJSDirector.getSoundGroups(sound),
+      sounds = group.sounds;
+
   soundGroups.push(group);
-  group.sounds.push(sound);
+  sounds.push(sound);
+  sounds[sound.id || sound.src] = sound;
+
 };
 
 // Remove @sound from @group.
 SoundJSDirector.unsetSoundGroup = SoundJSDirector.prototype.unsetSoundGroup =
 function unsetSoundGroup (sound, group) {
+
   if (!(sound && 'object' === typeof sound)) {
     return;
   }
+
   if ('string' === typeof group) {
     group = SoundJSDirector.group(group);
   }
+
   var soundGroups = SoundJSDirector.getSoundGroups(sound),
       groupSounds = group.sounds,
       index;
+
   while ((index = soundGroups.indexOf(group)) >= 0) {
     soundGroups.splice(index, 1);
   }
+
   while ((index = groupSounds.indexOf(sound)) >= 0) {
     groupSounds.splice(index, 1);
   }
+
+  var id = sound.id,
+      src = sound.src;
+
+  if (sound === groupSounds[id]) {
+    groupSounds[id] = null;
+    delete groupSounds[id];
+  }
+
+  if (sound === groupSounds[src]) {
+    groupSounds[src] = null;
+    delete groupSounds[src];
+  }
+  
 };
