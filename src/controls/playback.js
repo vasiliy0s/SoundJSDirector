82ec859d81ad7a8a2b959930a3221a26fee3e557
Vasiliy Os <talk@vasiliy0s.com>
2014-10-09 16:36:11 +0400

    Added 'all' argument to .play() method of SoundJSDirector.Group. Completed .pause(), .resume(), .stop() methods of group playback.

--- src/controls/playback.js
+++ src/controls/playback.js
@@ -2,52 +2,66 @@
 
 // TODO: add easings to all of current methods.
 
-SoundJSDirectorGroupProto.play = function playGroup (options) {
+SoundJSDirectorGroupProto.play = function playGroup (options, all) {
   // TODO: Pass group options to every sounds considering every group of sound.
 
   options = SoundJSDirector.extend({}, options, this.options);
 
   if (!options.collapsed) {
-    this._playFreeSounds(options);
+    this._playSounds(options, all);
   }
   
   else {
-    this._playCollapsed(options);
+    this._playCollapsed(options, all);
   }
   
   return this;
 
 };
 
-SoundJSDirectorGroupProto._playFreeSounds = function (options) {
+// Play free/all sounds in group.
+SoundJSDirectorGroupProto._playSounds = function (options, all) {
   // TODO: pass arguments multiplied with all instance groups.
-  SoundJSDirector.each(this._wait, function (sound) {
-    sound.play(options);
-  });
+  SoundJSDirector.each(
+    all ? this.sounds : this._wait, 
+    function (sound) {
+      sound.play(options);
+    }
+  );
   return this;
 };
 
-SoundJSDirectorGroupProto._playCollapsed = function (options) {
+// Play first of free/all sounds.
+SoundJSDirectorGroupProto._playCollapsed = function (options, all) {
   // TODO: pass arguments multiplied with all instance groups.
-  var sound = SoundJSDirector.randomItem(this._wait);
+  var sound = SoundJSDirector.randomItem(all ? this.sounds : this._wait);
   if (sound) {
     sound.play(options);
   }
   return this;
 };
 
+// Pause all playing sounds.
 SoundJSDirectorGroupProto.pause = function pauseGroup () {
-  // TODO
+  SoundJSDirector.each(this._playing, function (sound) {
+    sound.pause();
+  });
   return this;
 };
 
+// Resume all paused group sounds.
 SoundJSDirectorGroupProto.resume = function resumeGroup () {
-  // TODO
+  SoundJSDirector.each(this._playing, function (sound) {
+    sound.resume();
+  });
   return this;
 };
 
+// Stop all playing group sounds.
 SoundJSDirectorGroupProto.stop = function stopGroup () {
-  // TODO
+  SoundJSDirector.each(this._playing, function (sound) {
+    sound.stop();
+  });
   return this;
 };
 
