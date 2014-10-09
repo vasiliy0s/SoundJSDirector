acb10b295f5d2432cf5beefe804b7e67305bc5c1
Vasiliy Os <talk@vasiliy0s.com>
2014-10-09 18:36:04 +0400

    Added SoundJSDirector.parseOptions() method for pass only options from given object.

--- src/vars/options.js
+++ src/vars/options.js
@@ -8,6 +8,23 @@ var DEFAULT_OPTIONS = {
   delay: 0,
   offset: 0,
   pan: 0,
-  collpsed: false,
-  priority: 0, // not supported yet.
+  collapsed: false,
+  // priority: 0, // TODO: process in collapsed groups.
+  // offsets: [0], // TODO: process offsets for every playing sound
+};
+
+var DEFAULT_OPTIONS_KEYS = Object.keys(DEFAULT_OPTIONS);
+
+// Returns object with only known options.
+SoundJSDirector.parseOptions = function (obj) {
+  var options = {};
+  if ('object' === typeof obj) {
+    for (var i = DEFAULT_OPTIONS_KEYS.length, key; i--; ) {
+      key = DEFAULT_OPTIONS_KEYS[i];
+      if (obj.hasOwnProperty(key)) {
+        options[key] = obj[key];
+      }
+    }
+  }
+  return options;
 };
