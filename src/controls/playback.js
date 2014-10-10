6090a4b06d4b7720a6aa168b0930a13ee8d2181f
Vasiliy Os <talk@vasiliy0s.com>
2014-10-10 13:44:30 +0400

    Improved comments of .play() methods and improved arguments processing.

--- src/controls/playback.js
+++ src/controls/playback.js
@@ -3,10 +3,17 @@
 // TODO: add easings to all of current methods.
 
 // TODO: Pass group options to every sounds considering every group of sound.
-  
-SoundJSDirectorGroupProto.play = function playGroup (options, all) {
 
-  var arg0 = arguments[0], id;
+// Play instances (free/all/one by id/src) with @options.
+// Usages:
+// .play() - play all free sounds with group options.
+// .play(id) - play only one instance (if exists) from group by id or src with group options.
+// .play(id, options) - play only one instance (if exists) from group by id or src with custom @options extended by group options.
+// .play(options, true) - play all (of group) sounds with @options extened by gropu options; @options can contain .id or .src for play only defined sound.
+// .play(options) - play all free sounds with @options extended by group options; @options can contain .id or .src for play only defined sound; @options can contain .all property for select playing sounds from all group sounds (not only free sounds).
+SoundJSDirectorGroupProto.play = function playGroup () {
+
+  var arg0 = arguments[0], id, options, all = false;
 
   switch (arguments.length) {
 
@@ -21,18 +28,22 @@ SoundJSDirectorGroupProto.play = function playGroup (options, all) {
       else if ('object' !== typeof arg0) {
         throw 'SoundJSDirector.Group.play() given bad arguments';
       }
+      else {
+        options = arg0;
+        all = arg1;
+      }
     } break;
     
     case 1: {
       switch (typeof arg0) {
-        case 'string': id = arg0; options = all = false; break;
-        case 'object': id = arg0.id || arg0.src; all = false; break;
-        case 'boolean': all = arg0; options = id = false; break;
+        case 'string': id = arg0; break;
+        case 'object': options = arg0; id = options.id || options.src; break;
+        case 'boolean': all = arg0; break;
         default: throw 'SoundJSDirector.Group.play() given bad arguments';
       }
     } break;
     
-    case 0: all = options = id = false; break;
+    case 0: break;
   }
 
   var _options = SoundJSDirector.extend(
