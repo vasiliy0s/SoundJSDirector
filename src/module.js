eb0f31ac1130c7255ec30db761384c4b13f249f0
Vasiliy Os <talk@vasiliy0s.com>
2014-10-10 15:02:08 +0400

    Actualized TODOs in src/module.js

--- src/module.js
+++ src/module.js
@@ -1,12 +1,11 @@
 'use strict';
 
+// TODO: add easings to all of current methods.
 // TODO: add README.md with basic functionality.
 // TODO: extend README.md to full documentation.
-// TODO: SoundJSDirector.setGroups() - register groups of sounds.
-// TODO: SoundJSDirector.setSounds() - register sounds (with initialization or loading).
-// TODO: Process priorities of every sound with group.
+// TODO: Process priorities of every sound in group for collapsed groups playing.
 // TODO: provide sounds events in group like 'playing', 'played', 'loaded', other...
-// TODO: process INTERRUPTs on play.
+// TODO: pass Sound.INTERRUPT's to play() method.
 
 function SoundJSDirector () {
   throw 'SoundJSDirector cannot be initialized. Use \'new SoundJSDirector.Group\' for manage sounds';
