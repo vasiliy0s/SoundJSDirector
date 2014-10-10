74fdc8bab14df4b4f61b98348f455722eeaa2bdc
Vasiliy Os <talk@vasiliy0s.com>
2014-10-10 11:51:24 +0400

    Completed 'pan' property manipulations with .setPan(), .getPan() and .getSoundPan() methods of SoundJSDirector.Group.

--- src/controls/pan.js
+++ src/controls/pan.js
@@ -1,6 +1,32 @@
 'use strict';
 
-SoundJSDirectorGroupProto.setPan = function setGroupPan () {
-  // TODO
+// Set 'pan' property @value for playing/@all sounds in group
+// but every sound (instance) considers all of its groups.
+SoundJSDirectorGroupProto.setPan = function setGroupPan (value, all) {
+  value = parseFloat(value) || 0.0;
+  this.options.pan = value;
+  SoundJSDirector.each(
+    all ? this.sounds : this._playing,
+    function (sound) {
+      sound.setPan(this.getSoundPan(sound));
+    }, 
+    this
+  );
+  
   return this;
+};
+
+// Get 'pan' property of current group.
+SoundJSDirectorGroupProto.getPan = function () {
+  return this.options.pan;
+};
+
+// Compute 'pan' property of @sound.
+SoundJSDirectorGroupProto.getSoundPan = function getSoundPan (sound) {
+  var pan = 0.0, count = 0;
+  SoundJSDirector.eachSoundGroup(sound, function (group) {
+    pan += 1.0 + parseFloat(group.options.pan) || 0.0;
+    count++;
+  });
+  return pan - count;
 };
