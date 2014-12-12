a832dcf65db50d025907dc765ac0269790619293
Vasiliy Os <talk@vasiliy0s.com>
2014-12-12 16:28:23 +0300

    Added 'basePath' option to default.

--- src/vars/options.js
+++ src/vars/options.js
@@ -9,6 +9,7 @@ var DEFAULT_OPTIONS = {
   offset: 0,
   pan: 0,
   collapsed: false,
+  basePath: '',
   // priority: 0, // TODO: process in collapsed groups.
   // offsets: [0], // TODO: process offsets for every playing sound
 };
